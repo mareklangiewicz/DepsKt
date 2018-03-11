a5c4bcbda6e7752e8c50d1d1632ae1d4ad2c77ec
Marek Langiewicz <marek.langiewicz@gmail.com>
2018-03-11 19:52:44 +0100

    reformat

--- buildSrc/src/main/java/deps.kt
+++ buildSrc/src/main/java/deps.kt
@@ -1,9 +1,10 @@
+@file:Suppress("unused")
+
 /**
- * Common dependencies for typical java/kotlin/android projects
+ * Common dependencies versions for java/kotlin/android projects
  *
  * @see <a href="https://github.com/langara/deps.kt">https://github.com/langara/deps.kt</a>
  */
-
 object Vers {
     val kotlinMajor = 1
     val kotlinMinor = 2
@@ -42,12 +43,11 @@ object Vers {
     val androidTestRunnerClass = "android.support.test.runner.AndroidJUnitRunner"
 }
 
-private fun dep(group: String, name: String, version: String) = mapOf(
-        "group" to group,
-        "name" to name,
-        "version" to version
-)
-
+/**
+ * Common dependencies for java/kotlin/android projects
+ *
+ * @see <a href="https://github.com/langara/deps.kt">https://github.com/langara/deps.kt</a>
+ */
 object Deps {
     val kotlinGradlePlugin = dep("org.jetbrains.kotlin", "kotlin-gradle-plugin", Vers.kotlin)
     val androidGradlePlugin = dep("com.android.tools.build", "gradle", Vers.androidGradlePlugin)
@@ -94,4 +94,9 @@ object Deps {
     val androidTestRules = dep("com.android.support.test", "rules", Vers.androidSupportTest)
 }
 
+private fun dep(group: String, name: String, version: String) = mapOf(
+        "group" to group,
+        "name" to name,
+        "version" to version
+)
 
