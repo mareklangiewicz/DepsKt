4335ba472b8a59e3212d631f4e0b32311a9f080f
Marek Langiewicz <marek.langiewicz@gmail.com>
2020-11-27 12:11:31 +0100

    simplify build file

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,19 +1,2 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-plugins { `kotlin-dsl`
-    kotlin("jvm") version "1.3.72"
-}
-repositories {
-    jcenter()
-}
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-}
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
+plugins { `kotlin-dsl` }
+repositories { jcenter() }
