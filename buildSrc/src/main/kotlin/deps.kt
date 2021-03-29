6569b14d9bb132c76afe267d8544734190acc6ea
Marek Langiewicz <marek.langiewicz@gmail.com>
2021-03-29 18:49:28 +0200

    update

--- buildSrc/src/main/kotlin/deps.kt
+++ buildSrc/src/main/kotlin/deps.kt
@@ -123,7 +123,7 @@ object Vers {
     const val dbusJava = "3.3.0"
         // https://github.com/hypfvieh/dbus-java
 
-    const val dbusKotlin = "0.0.02"
+    const val dbusKotlin = "0.0.04"
     // https://github.com/langara/dbus-kotlin
 
     const val javaWebsocket = "1.5.1"
