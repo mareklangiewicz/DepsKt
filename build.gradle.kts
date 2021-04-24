3288344741895c187af7ef5046d3c4659053e1cf
Marek Langiewicz <marek.langiewicz@gmail.com>
2021-04-24 22:02:38 +0200

    Add gradle portal publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
 plugins {
+    id("com.gradle.plugin-publish") version "0.14.0"
+        // https://plugins.gradle.org/docs/publish-plugin
     `java-gradle-plugin`
     `kotlin-dsl`
-    `maven-publish`
+}
+
+repositories {
+    mavenCentral()
 }
 
 group = "pl.mareklangiewicz.deps"
@@ -20,6 +25,41 @@ gradlePlugin {
     }
 }
 
-repositories {
-    gradlePluginPortal() // so that external plugins can be resolved in dependencies section
+pluginBundle {
+    // These settings are set for the whole plugin bundle
+    vcsUrl = "https://github.com/langara/deps.kt"
+
+    // tags and description can be set for the whole bundle here, but can also
+    // be set / overridden in the config for specific plugins
+    description = "Updated dependencies for typical java/kotlin/android projects (with IDE support)."
+
+    // The plugins block can contain multiple plugin entries.
+    //
+    // The name for each plugin block below (greetingsPlugin, goodbyePlugin)
+    // does not affect the plugin configuration, but they need to be unique
+    // for each plugin.
+
+    // Plugin config blocks can set the id, displayName, version, description
+    // and tags for each plugin.
+
+    // id and displayName are mandatory.
+    // If no version is set, the project version will be used.
+    // If no tags or description are set, the tags or description from the
+    // pluginBundle block will be used, but they must be set in one of the
+    // two places.
+
+    (plugins) {
+
+        // first plugin
+        "depsPlugin" {
+            // id is captured from java-gradle-plugin configuration
+            displayName = "Deps.kt plugin"
+        }
+
+        // another plugin
+        "depsSettingsPlugin" {
+            // id is captured from java-gradle-plugin configuration
+            displayName = "Depps.kt settings plugin"
+        }
+    }
 }
