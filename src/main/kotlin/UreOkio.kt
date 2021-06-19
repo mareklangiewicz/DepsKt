cbbda8999a699ab71311f1f85a7f8966ee2c9af6
Marek Langiewicz <marek.langiewicz@gmail.com>
2021-06-19 11:49:53 +0200

    experiments

--- src/main/kotlin/UreOkio.kt
+++ src/main/kotlin/UreOkio.kt
@@ -35,37 +35,57 @@ fun commentOutMultiplatformFunInFileTree(path: Path) =
 fun undoCommentOutMultiplatformFunInFileTree(path: Path) =
     forAllKtFiles(path) { undoCommentOutMultiplatformFunInFile(it) }
 
+/**
+ * @param inputPath path of input file
+ * @param outputPath path of output file - can be the same as inputPath;
+ * nothing is written to file system if it's null;
+ * @param process file content transformation; if it returns null - outputPath is not even touched
+ */
+fun FileSystem.processFile(inputPath: Path, outputPath: Path? = null, process: (String) -> String?) {
+
+    val input = source(inputPath).buffer().use { it.readUtf8() }
+
+    val output = process(input)
+
+    if (outputPath == null) {
+        if (output != null) println("Warning: ignoring non-null output because outputPath is null")
+        return
+    }
+
+    if (output == null) {
+        println("Ignoring outputPath because output to write is null")
+        return
+    }
+
+    sink(outputPath).buffer().use { it.writeUtf8(output) }
+}
 
 fun FileSystem.commentOutMultiplatformFunInFile(file: Path) {
     println("\ncommenting: $file") // FIXME:remove/ulog
 
-    val input = source(file).buffer().use { it.readUtf8() }
+    processFile(file, file) { input ->
 
-    val output1 = ureNotCommentedOutArea(ureExpectFun)
-        .compile(MULTILINE)
-        .replace(input) { "/*\n${it.value}\n*/" }
+        val output1 = ureNotCommentedOutArea(ureExpectFun)
+            .compile(MULTILINE)
+            .replace(input) { "/*\n${it.value}\n*/" }
 
-    val output2 = ir("actual fun")
-        .compile()
-        .replace(output1) { "/*actual*/ fun" }
-
-    sink(file).buffer().use { it.writeUtf8(output2) }
+        ir("actual fun").compile().replace(output1) { "/*actual*/ fun" }
+    }
 }
 
 fun FileSystem.undoCommentOutMultiplatformFunInFile(file: Path) {
     println("\nundo comments: $file") // FIXME:remove/ulog
 
-    val input = source(file).buffer().use { it.readUtf8() }
+    processFile(file, file) { input ->
 
-    val myFun = named("myFun") { 1 of ureExpectFun }
+        val myFun = named("myFun") { 1 of ureExpectFun }
 
-    val output1 = ureCommentedOutArea(myFun)
-        .compile(MULTILINE)
-        .replace(input) { it.groups["myFun"]!!.value }
+        val output1 = ureCommentedOutArea(myFun)
+            .compile(MULTILINE)
+            .replace(input) { it.groups["myFun"]!!.value }
 
-    val output2 = ir("/\\*actual\\*/ fun")
-        .compile()
-        .replace(output1) { "actual fun" }
-
-    sink(file).buffer().use { it.writeUtf8(output2) }
+        ir("/\\*actual\\*/ fun")
+            .compile()
+            .replace(output1) { "actual fun" }
+    }
 }
