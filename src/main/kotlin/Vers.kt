7689093bce6391dbe1ebc9f0299d1fa584d7ce63
Marek Langiewicz <marek.langiewicz@gmail.com>
2021-11-07 16:13:28 +0100

    experiments

--- src/main/kotlin/Vers.kt
+++ src/main/kotlin/Vers.kt
@@ -25,7 +25,7 @@ object Vers {
     // just a reference - not useful in typical cases
     const val gradle5 = "5.6.4"
     const val gradle6 = "6.8.3"
-    const val gradle7 = "7.2"
+    const val gradle7 = "7.3-rc-3"
     const val gradle = gradle7
     // https://gradle.org/releases/
     // https://services.gradle.org/versions
