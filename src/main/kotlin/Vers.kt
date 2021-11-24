f2a4960ccc82246ad59c2446f337676e99f39736
Marek Langiewicz <marek.langiewicz@gmail.com>
2021-11-24 11:45:28 +0100

    experiments

--- src/main/kotlin/Vers.kt
+++ src/main/kotlin/Vers.kt
@@ -44,8 +44,8 @@ object Vers {
     // https://services.gradle.org/versions
     // https://services.gradle.org/versions/current
 
-    const val composeDesktopVerMain = "1.0.0-beta5"
-    const val composeDesktopVerEdge = "1.0.0-beta6-dev474"
+    const val composeDesktopVerMain = "1.0.0-rc3"
+    const val composeDesktopVerEdge = "1.0.0-rc3"
     const val composeDesktop = composeDesktopVerEdge
 
     // https://github.com/JetBrains/compose-jb
