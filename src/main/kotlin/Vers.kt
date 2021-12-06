b16687b188a2c2c2a0e3db1aeed6d25f69b8c9d5
Marek Langiewicz <marek.langiewicz@gmail.com>
2021-12-06 18:23:44 +0100

    fun

--- src/main/kotlin/Vers.kt
+++ src/main/kotlin/Vers.kt
@@ -275,7 +275,7 @@ object Vers {
     const val upue = "0.0.08"
     // https://github.com/langara/upue/releases
 
-    const val kommandLine = "0.0.02"
+    const val kommandLine = "0.0.04"
     // https://github.com/langara/kommandline/releases
 
     const val sandboxui = "0.0.5"
