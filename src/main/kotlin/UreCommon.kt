28108fb8c2e894602a69d1925d1af5ee5b263ddf
Marek Langiewicz <marek.langiewicz@gmail.com>
2022-01-23 13:45:09 +0100

    fun

--- src/main/kotlin/UreCommon.kt
+++ src/main/kotlin/UreCommon.kt
@@ -2,47 +2,11 @@ package pl.mareklangiewicz.ure
 
 import kotlin.text.RegexOption.*
 
-fun ureWhateva(reluctant: Boolean = true) = ure { x(0..MAX, reluctant = reluctant) of any }
-    .withOptionsEnabled(DOT_MATCHES_ALL) // Here I explicitly set DOT_MATCHES_ALL as a special case.
-        // By default we assume this option is disabled. (see comment at Ure.compile())
-
-fun ureAnyLine(withLF: Boolean = true, withCR: Boolean = false) = ure {
-    1 of BOL
-    0..MAX of any
-    1 of EOL
-    if (withCR) 1 of cr
-    if (withLF) 1 of lf
-}.withOptionsDisabled(DOT_MATCHES_ALL) // Here I explicitly disable DOT_MATCHES_ALL as a special case.
-    // Normally we just assume this option is disabled anyway. (see comment at Ure.compile())
-
-
-fun ureNotCommentedOutArea(area: Ure, maxSpacesBehind: Int = 100) = ure {
-    1 of lookBehind(positive = false) {
-        1 of BOL
-        1 of ir("/\\*")
-        0..maxSpacesBehind of space // Can not use MAX - java look-behind implementation complains (throws)
+fun ureWhateva(reluctant: Boolean = true, inLine: Boolean = false) =
+    ure { x(0..MAX, reluctant = reluctant) of any }.run {
+        if (inLine) withOptionsDisabled(DOT_MATCHES_ALL)
+        else withOptionsEnabled(DOT_MATCHES_ALL)
     }
-    1 of area
-    1 of lookAhead(positive = false) {
-        0..MAX of space
-        1 of ir("\\*/")
-        1 of EOL
-    }
-}
-
-fun ureCommentedOutArea(area: Ure) = ure {
-    1 of {
-        1 of BOL
-        1 of ir("/\\*")
-        0..MAX of space
-    }
-    1 of area
-    1 of {
-        0..MAX of space
-        1 of ir("\\*/")
-        1 of EOL
-    }
-}
 
 fun ureBlankStartOfLine() = ure {
     1 of BOL
@@ -56,21 +20,37 @@ fun ureBlankRestOfLine(withOptCR: Boolean = true, withOptLF: Boolean = true) = u
     if (withOptLF) 0..1 of lf
 }
 
-fun ureSimpleCommentLine(ureContent: Ure) = ure {
+fun ureLineWithContent(content: Ure, withOptCR: Boolean = true, withOptLF: Boolean = true) = ure {
     1 of ureBlankStartOfLine()
-    1 of ir("//")
+    1 of content
+    1 of ureBlankRestOfLine(withOptCR, withOptLF)
+}
+
+fun ureAnyLine(withOptCR: Boolean = true, withOptLF: Boolean = true) =
+    ureLineWithContent(ureWhateva(inLine = true), withOptCR, withOptLF)
+
+fun Ure.withOptSpacesAround() = ure {
     0..MAX of spaceInLine
-    1 of ureContent
-    1 of ureBlankRestOfLine()
+    1 of this
+    0..MAX of spaceInLine
+}
+
+fun ureCommentLine(content: Ure = ureWhateva(inLine = true), traditional: Boolean = false) = ure {
+    val something = ure {
+        1 of if (traditional) ir("/\\*") else ir("//")
+        1 of content.withOptSpacesAround()
+        if (traditional) 1 of ir("\\*/")
+    }
+    1 of ureLineWithContent(something)
 }
 
 fun ureRegion(content: Ure, regionName: Ure? = null) = ure {
-    1 of ureSimpleCommentLine(ureKeywordAndOptArg(ir("region"), regionName))
+    1 of ureCommentLine(ureKeywordAndOptArg(ir("region"), regionName))
     1 of content
-    1 of ureSimpleCommentLine(ureKeywordAndOptArg(ir("endregion"), regionName))
+    1 of ureCommentLine(ureKeywordAndOptArg(ir("endregion"), regionName))
 }
 
-private fun ureKeywordAndOptArg(
+fun ureKeywordAndOptArg(
     keyword: Ure,
     arg: Ure? = null,
     separator: Ure = ure { 1..MAX of spaceInLine },
@@ -79,6 +59,28 @@ private fun ureKeywordAndOptArg(
     arg?.let {
         1 of separator
         1 of it
+    }
+}
+
+
+private val ureCommentedOutAreaStartLine = ureLineWithContent(ir("/\\*"))
+private val ureCommentedOutAreaEndLine = ureLineWithContent(ir("\\*/"))
+
+fun ureCommentedOutArea(area: Ure = ureWhateva()) = ure {
+    1 of ureCommentedOutAreaStartLine
+    1 of area
+    1 of ureCommentedOutAreaEndLine
+}
+
+fun ureNotCommentedOutArea(area: Ure = ureWhateva(reluctant = false), maxSpacesBehind: Int = 100) = ure {
+    1 of lookBehind(positive = false) {
+        1 of ir("/\\*")
+        0..maxSpacesBehind of space // Can not use MAX - java look-behind implementation complains (throws)
+    }
+    1 of area
+    1 of lookAhead(positive = false) {
+        0..MAX of space
+        1 of ir("\\*/")
     }
 }
 
