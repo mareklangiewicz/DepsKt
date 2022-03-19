862df64ee21347130959b21b6d2ee5e1013ceeda
Marek Langiewicz <marek.langiewicz@gmail.com>
2022-03-19 11:51:59 +0100

    fun

--- src/main/kotlin/deps/Repos.kt
+++ src/main/kotlin/deps/Repos.kt
@@ -1,9 +1,18 @@
 package pl.mareklangiewicz.deps
 
 object Repos {
-    const val ossSonatypeSnapshots = "https://oss.sonatype.org/content/repositories/snapshots"
+
+    // https://central.sonatype.org/publish/publish-guide/#accessing-repositories
+    const val sonatypeOssSnapshotsOld = "https://oss.sonatype.org/content/repositories/snapshots/"
+    const val sonatypeOssSnapshotsNew = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+    const val sonatypeOssSnapshots = sonatypeOssSnapshotsNew
+    const val sonatypeOssNexus = "https://s01.oss.sonatype.org/service/local/"
+
     const val ktorEap = "https://maven.pkg.jetbrains.space/public/p/ktor/eap"
+
     const val composeDesktopDev = "https://maven.pkg.jetbrains.space/public/p/compose/dev"
+
     const val jitpack = "https://jitpack.io"
+
     const val kotlinx = "https://kotlin.bintray.com/kotlinx"
 }
