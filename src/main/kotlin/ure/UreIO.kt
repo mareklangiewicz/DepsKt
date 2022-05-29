d5128cb3419e5bfa672055bbb0a2cdefb37056a1
Marek Langiewicz <marek.langiewicz@gmail.com>
2022-05-29 09:45:28 +0200

    fun

--- src/main/kotlin/ure/UreIO.kt
+++ src/main/kotlin/ure/UreIO.kt
@@ -4,14 +4,6 @@ import pl.mareklangiewicz.io.*
 import okio.*
 
 
-fun FileSystem.commentOutMultiplatformFunInEachKtFile(root: Path) =
-    findAllFiles(root).filterExt("kt").forEach { commentOutMultiplatformFunInFile(it) }
-
-fun FileSystem.undoCommentOutMultiplatformFunInEachKtFile(root: Path) =
-    findAllFiles(root).filterExt("kt").forEach { undoCommentOutMultiplatformFunInFile(it) }
-
-
-
 fun FileSystem.readAndMatchUre(file: Path, vararg opts: RegexOption, init: UreProduct.() -> Unit): MatchResult? =
     readAndMatchUre(file, ure(*opts) { init() })
 
@@ -19,28 +11,16 @@ fun FileSystem.readAndMatchUre(file: Path, ure: Ure): MatchResult? = readUtf8(fi
 
 fun FileSystem.commentOutMultiplatformFunInFile(file: Path) {
     println("\ncommenting: $file") // FIXME:remove/ulog
-
-    processFile(file, file) { input ->
-
-        val output1 = ureExpectFun.notCommentedOut().compile()
-            .replace(input) { "/*\n${it.value}\n*/" }
-
-        val output2 = ir("actual fun").compile().replace(output1) { "/*actual*/ fun" }
-
-        ir("actual suspend fun").compile().replace(output2) { "/*actual*/ suspend fun" }
-        // FIXME: merge this two replaces to one using better URE
-    }
+    processFile(file, file, String::commentOutMultiplatformFun)
 }
 
 fun FileSystem.undoCommentOutMultiplatformFunInFile(file: Path) {
     println("\nundo comments: $file") // FIXME:remove/ulog
-
-    processFile(file, file) { input ->
-
-        val myFun = ure("myFun") { 1 of ureExpectFun }
-
-        val output1 = myFun.commentedOut().compile().replace(input) { it["myFun"] }
-
-        ir("/\\*actual\\*/").compile().replace(output1) { "actual" }
-    }
+    processFile(file, file, String::undoCommentOutMultiplatformFun)
 }
+
+fun FileSystem.commentOutMultiplatformFunInEachKtFile(root: Path) =
+    findAllFiles(root).filterExt("kt").forEach { commentOutMultiplatformFunInFile(it) }
+
+fun FileSystem.undoCommentOutMultiplatformFunInEachKtFile(root: Path) =
+    findAllFiles(root).filterExt("kt").forEach { undoCommentOutMultiplatformFunInFile(it) }
--- src/main/kotlin/ure/UreMulti.kt
+++ src/main/kotlin/ure/UreMulti.kt
@@ -1,5 +1,18 @@
 package pl.mareklangiewicz.ure
 
+fun String.commentOutMultiplatformFun(): String {
+    val output1 = ureExpectFun.notCommentedOut().compile().replace(this) { "/*\n${it.value}\n*/" }
+    val output2 = ir("actual fun").compile().replace(output1) { "/*actual*/ fun" }
+    return ir("actual suspend fun").compile().replace(output2) { "/*actual*/ suspend fun" }
+    // FIXME_maybe: merge this two replaces to one using better URE
+}
+
+fun String.undoCommentOutMultiplatformFun(): String {
+    val myFun = ure("myFun") { 1 of ureExpectFun }
+    val output1 = myFun.commentedOut().compile().replace(this) { it["myFun"] }
+    return ir("/\\*actual\\*/").compile().replace(output1) { "actual" }
+}
+
 // TODO: refactor - also check where should I change space to spaceInLine
 val ureExpectFun = ure {
     val keyword = ure {
