c9f960598b46aac8c69e079198bd38a0f2cf332a
Marek Langiewicz <marek.langiewicz@gmail.com>
2022-07-21 13:20:14 +0200

    fun

--- src/test/kotlin/UreTemplatesTests.kt
+++ src/test/kotlin/UreTemplatesTests.kt
@@ -9,25 +9,29 @@ class UreTemplatesTests {
 
     @TestFactory
     fun ureTemplatesTestFactory() = uspekTestFactory {
-        "check all build regions sync" o {
-            checkAllKnownRegionsSynced()
-        }
-        "check all known regions in KommandLine project" ox {
-            val myKotlinSrcCodeRoot = "/home/marek/code/kotlin/KommandLine".toPath()
-            SYSTEM.checkAllKnownRegionsInAllFoundFiles(myKotlinSrcCodeRoot, verbose = true)
-        }
-        "check all known regions in all kotlin projects" ox {
-            val myKotlinSrcCodeRoot = "/home/marek/code/kotlin".toPath()
-            SYSTEM.checkAllKnownRegionsInAllFoundFiles(myKotlinSrcCodeRoot, verbose = true)
-        }
-
-        "DANGEROUS inject all build regions to sync" ox {
-            injectAllKnownRegionsToSync()
-        }
-
-        "DANGEROUS inject all known regions to KommandLine project" ox {
-            val myKotlinSrcCodeRoot = "/home/marek/code/kotlin/KommandLine".toPath()
-            SYSTEM.injectAllKnownRegionsToAllFoundFiles(myKotlinSrcCodeRoot)
-        }
+        oCheckAllKnownRegionsSynced()
+        oCheckAllKnownRegionsInKotlinProject("KommandLine")
+        oCheckAllKnownRegionsInKotlinProject("AbcdK")
+        // oCheckAllKnownRegionsInAllKotlinProjects()
+        // oDangerousInjectAllKnownRegionsToSync()
+        // oDangerousInjectAllKnownRegionsToKotlinProject("AbcdK")
     }
+
 }
+
+private val pathToKotlinProjects = "/home/marek/code/kotlin".toPath()
+
+private fun oCheckAllKnownRegionsSynced() = "check all known regions synced" o { checkAllKnownRegionsSynced() }
+private fun oCheckAllKnownRegionsInAllKotlinProjects() = "check all known regions in all kotlin projects" o {
+    SYSTEM.checkAllKnownRegionsInAllFoundFiles(pathToKotlinProjects, verbose = true)
+}
+private fun oCheckAllKnownRegionsInKotlinProject(projectDir: String) =
+    "check all known regions in $projectDir project" o {
+        SYSTEM.checkAllKnownRegionsInAllFoundFiles(pathToKotlinProjects / projectDir, verbose = true)
+    }
+private fun oDangerousInjectAllKnownRegionsToKotlinProject(projectDir: String) =
+    "DANGEROUS inject all known regions to $projectDir project" o {
+        SYSTEM.injectAllKnownRegionsToAllFoundFiles(pathToKotlinProjects / projectDir)
+    }
+private fun oDangerousInjectAllKnownRegionsToSync() =
+    "DANGEROUS inject all known regions to sync" o { injectAllKnownRegionsToSync() }
