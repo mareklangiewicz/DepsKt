7f2f41c1c3fbfe3123e32c9b54480b530af5fe35
Marek Langiewicz <marek.langiewicz@gmail.com>
2022-08-04 10:36:27 +0200

    fun

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 dependencies {
     api("com.squareup.okio:okio:3.2.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
-    implementation("com.android.library:com.android.library.gradle.plugin:7.4.0-alpha08")
+    implementation("com.android.library:com.android.library.gradle.plugin:7.4.0-alpha09")
         // Warning: andro gradle plugin is needed here, so kotlin plugin can access BaseExtension class etc.
         // It is needed in android projects using deps.kt (which does: plugins { kotlin("android") }
         // It doesn't work if users just add this andro gradle plugin dependency in their projects.
