d403839b5fd4b071dbd535e3cb087c49e096a4ac
Marek Langiewicz <marek.langiewicz@gmail.com>
2023-06-19 20:38:19 +0200

    fun

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import pl.mareklangiewicz.deps.*
+import pl.mareklangiewicz.utils.includeAndSubstituteBuild
 
 rootProject.name = "DepsKt"
 
@@ -10,7 +12,7 @@ pluginManagement {
 }
 
 plugins {
-    id("pl.mareklangiewicz.deps.settings") version "0.2.38" // https://plugins.gradle.org/search?term=mareklangiewicz
+    id("pl.mareklangiewicz.deps.settings") version "0.2.40" // https://plugins.gradle.org/search?term=mareklangiewicz
     id("com.gradle.enterprise") version "3.13.3" // https://docs.gradle.com/enterprise/gradle-plugin/
 }
 
