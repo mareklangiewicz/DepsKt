fcdf19bfe27b8f5e7321778d050dfdc9c442a303
Marek Langiewicz <marek.langiewicz@gmail.com>
2023-06-23 17:23:48 +0200

    fun

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 
 dependencies {
     api(Com.SquareUp.Okio.okio)
-    api(Langiewicz.kommandline.withVer(Ver(0, 0, 15)))
+    api(Langiewicz.kommandline.withVer(Ver(0, 0, 16)))
         // FIXME: remove manual version when published deps catch up
     implementation(Io.GitHub.TypeSafeGitHub.github_workflows_kt)
     testImplementation(Langiewicz.uspekx_junit5)
