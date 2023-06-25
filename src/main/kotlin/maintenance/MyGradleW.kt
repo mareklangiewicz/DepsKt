0a0fa06fd3e7c2c23f25d80aa3d97cad864f4f03
Marek Langiewicz <marek.langiewicz@gmail.com>
2023-06-25 13:12:27 +0200

    fun

--- src/main/kotlin/maintenance/MyGradleW.kt
+++ src/main/kotlin/maintenance/MyGradleW.kt
@@ -13,7 +13,7 @@ val gradlewRelPaths =
 private val MyNonGradlewProjects = listOf("MyScripts", "uspek-js-playground", "uspek-painters")
 private val MyGradlewSubProjects =
     listOf("template-mpp", "template-andro", "sample-sourcefun").map { "DepsKt/$it" } +
-        listOf("isolatedground1", "isolatedground2", "isolatedground3", "isolatedkamera").map { "kokpit667/$it" }
+        listOf("isolatedground1", "isolatedground2", "isolatedground3", "isolatedkamera").map { "kokpit667/isolated/$it" }
 
 private val MyGradlewProjects = MyKotlinProjects - MyNonGradlewProjects + MyGradlewSubProjects
 
