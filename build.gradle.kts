be2fe8dceaba6150bad34e6b3d3c36684c15ddb6
Marek Langiewicz <marek.langiewicz@gmail.com>
2023-08-24 10:03:54 +0200

    fun

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ defaultGroupAndVerAndDescription(
         group = "pl.mareklangiewicz.deps", // important non default ...deps group (as accepted on gradle portal)
         description = "Updated dependencies for typical java/kotlin/android projects (with IDE support).",
         githubUrl = "https://github.com/langara/DepsKt",
-        version = Ver(0, 2, 53),
+        version = Ver(0, 2, 54),
         // https://plugins.gradle.org/search?term=pl.mareklangiewicz
     )
 )
