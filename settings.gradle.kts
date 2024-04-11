5f22d498427232d883a41a0870abd2f2598712ca
Marek Langiewicz <marek.langiewicz@gmail.com>
2024-04-11 11:26:40 +0200

    fun

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ pluginManagement {
 
 plugins {
   id("pl.mareklangiewicz.deps.settings") version "0.2.97" // https://plugins.gradle.org/search?term=mareklangiewicz
-  id("com.gradle.enterprise") version "3.17.1" // https://docs.gradle.com/enterprise/gradle-plugin/
+  id("com.gradle.develocity") version "3.17.1" // https://docs.gradle.com/enterprise/gradle-plugin/
 }
 
 // includeAndSubstituteBuild("../KommandLine", Langiewicz.kommandline.mvn, ":kommandline")
