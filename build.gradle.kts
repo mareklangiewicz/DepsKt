11a577d7bd8c2835db9d1b7030f7bd2708137d0a
Marek Langiewicz <marek.langiewicz@gmail.com>
2024-05-01 18:17:30 +0200

    fun

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ repositories {
 }
 
 dependencies {
-  implementation(Langiewicz.kgroundx_maintenance)
+  api(Langiewicz.kgroundx_maintenance)
   testImplementation(Langiewicz.uspekx_junit5)
   testImplementation(Org.JUnit.Jupiter.junit_jupiter)
   testImplementation(Org.JUnit.Jupiter.junit_jupiter_engine)
@@ -59,7 +59,7 @@ defaultGroupAndVerAndDescription(
     group = "pl.mareklangiewicz.deps", // important non default ...deps group (as accepted on gradle portal)
     description = "Updated dependencies for typical java/kotlin/android projects (with IDE support).",
     githubUrl = "https://github.com/mareklangiewicz/DepsKt",
-    version = Ver(0, 3, 3),
+    version = Ver(0, 3, 4),
     // https://plugins.gradle.org/search?term=pl.mareklangiewicz
     settings = LibSettings(
       withJs = false,
