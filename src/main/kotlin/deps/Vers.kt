3f15dea085f815f41f6fe009f80c1af7d7c5499e
Marek Langiewicz <marek.langiewicz@gmail.com>
2024-09-20 09:48:04 +0200

    fun

--- src/main/kotlin/deps/Vers.kt
+++ src/main/kotlin/deps/Vers.kt
@@ -126,7 +126,7 @@ object Vers {
    * [gradle portal](https://plugins.gradle.org/plugin/org.jetbrains.kotlin.jupyter.api)
    * [github](https://github.com/Kotlin/kotlin-jupyter)
    */
-  val KotlinJupyterPlug = Ver("0.12.0-288")
+  val KotlinJupyterPlug = Ver("0.12.0-290")
 
   /**
    * Gradle Shadow Gradle Plugin
@@ -134,7 +134,7 @@ object Vers {
    * - [github john rengelman shadow](https://github.com/johnrengelman/shadow)
    * - [docs](https://github.com/johnrengelman/shadow)
    */
-  val GradleShadowPlug = Ver("8.3.1")
+  val GradleShadowPlug = Ver("8.3.2")
 
   /**
    * Osacky Doctor Gradle Plugin
@@ -151,7 +151,7 @@ object Vers {
    * - [plugins gradle deps settings](https://plugins.gradle.org/plugin/pl.mareklangiewicz.deps.settings)
    * - [plugins gradle search mareklangiewicz](https://plugins.gradle.org/search?term=pl.mareklangiewicz)
    */
-  val DepsPlug = Ver(0, 3, 49) // TODO make sure it's always synced with myLibDetails(version)
+  val DepsPlug = Ver(0, 3, 50) // TODO make sure it's always synced with myLibDetails(version)
 
   /**
    * SourceFun Gradle Plugin
