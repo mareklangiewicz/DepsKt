4a36cb74cfcd1e22590f58543f1e1e38a9b73915
Marek Langiewicz <marek.langiewicz@gmail.com>
2026-08-18 06:17:50 +0000

    Commit from GitHub Actions (Update Generated Deps)

--- src/main/kotlin/deps/Deps.kt
+++ src/main/kotlin/deps/Deps.kt
@@ -805,7 +805,7 @@ object Com {
       val desugar_jdk_libs = "com.android.tools" d "desugar_jdk_libs" w "2.1.5"
       val r8 = "com.android.tools" d "r8" w "9.4.14" w "9.5.5-dev"
       object Build {
-        val gradle = "com.android.tools.build" d "gradle" w "2.3.0" w "9.3.1" w "9.4.0-alpha08"
+        val gradle = "com.android.tools.build" d "gradle" w "2.3.0" w "9.3.1" w "9.4.0-rc01" w "9.5.0-alpha01"
       }
     }
   }
