c3b8d4cb9b1f453badc4e38c9cfa76cfa7391bbd
Marek Langiewicz <marek.langiewicz@gmail.com>
2026-08-22 06:14:48 +0000

    Commit from GitHub Actions (Update Generated Deps)

--- src/main/kotlin/deps/Deps.kt
+++ src/main/kotlin/deps/Deps.kt
@@ -1040,7 +1040,7 @@ object Com {
     }
     object Maps {
       object Android {
-        val maps_compose = "com.google.maps.android" d "maps-compose" w "8.4.0" w "9.0.0-rc01"
+        val maps_compose = "com.google.maps.android" d "maps-compose" w "8.5.0" w "9.0.0-rc01"
         val maps_ktx = "com.google.maps.android" d "maps-ktx" w "6.3.0"
         val maps_rx = "com.google.maps.android" d "maps-rx" w "1.0.1"
         val maps_utils = "com.google.maps.android" d "android-maps-utils" w "5.1.1" w "6.0.0-rc01"
