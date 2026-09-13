995e484581e646dbdbceb2692fb9550b8a9b86ac
Marek Langiewicz <marek.langiewicz@gmail.com>
2026-09-13 21:00:14 +0200

    fun

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -32,7 +32,7 @@ pluginManagement {
 
 plugins {
   id("pl.mareklangiewicz.deps.settings") version "0.4.24" // https://plugins.gradle.org/search?term=mareklangiewicz
-  id("com.gradle.develocity") version "4.5.0" // https://docs.gradle.com/develocity/gradle-plugin/
+  id("com.gradle.develocity") version "4.5.1" // https://docs.gradle.com/develocity/gradle-plugin/
 }
 
 develocity {
